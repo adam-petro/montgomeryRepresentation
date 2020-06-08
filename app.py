4a9b41fe894400c8d96248a20c23883c2785182b
Escavadora <patrik.bagar@gmail.com>
2020-06-08 22:11:21 +0200

    First working version

--- app.py
+++ app.py
@@ -9,11 +9,40 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 import sys
+import random
+import math
+#import unittest
+r = 64  #2^6
 
+def inverznyEuklid(a, m):
+    a = int(a)
+    m = int(m)
+    m0 = m
+    y = 0
+    x = 1
+
+    if int(m) == 1:
+        return 0
+
+    while int(a) > 1:
+        q = a // int(m)
+        t = m
+        m = a % int(m)
+        a = t
+        t = y
+
+        y = x-q*y
+        x = t
+
+    if x < 0:
+        x = x+m0
+
+    return x
 
 def convertToMontgomery(x, n):
-
-    result = str(int(x) * int(n))
+    #r = 64  # 2^6
+    res = (int(x)*int(r)) % int(n)
+    result = str(res)
     returnString = (
         f"<span style=\"font-size:20px\">x: {x}<br>n: {n}<br>result: {result}</span>"
     )
@@ -21,7 +50,9 @@ def convertToMontgomery(x, n):
 
 
 def convertFromMontgomery(x, n):
-    result = str(int(x) + int(n))
+    invR = inverznyEuklid(r, n)
+    res = (int(x)*int(invR)) % int(n)
+    result = str(res)
     returnString = (
         f"<span style=\"font-size:20px\">x: {x}<br>n: {n}<br>result: {result}</span>"
     )
@@ -135,10 +166,107 @@ class Ui_MainWindow(object):
         self.actionl.setText(_translate("MainWindow", "l;"))
 
 
+# class MontgomeryReducerTest(unittest.TestCase):
+#
+#     def test_basic(self):
+#         for _ in range(3000):
+#             bitlen = random.randint(2, 100)
+#             mod = random.randrange(1 << bitlen, 2 << bitlen) | 1  # Force it to be odd
+#             mr = MontgomeryReducer(mod)
+#
+#             for _ in range(100):
+#                 x = random.randrange(0, mod)
+#                 y = random.randrange(0, mod)
+#                 u = mr.convert_in(x)
+#                 v = mr.convert_in(y)
+#                 w = mr.multiply(u, v)
+#                 if mr.convert_out(w) != x * y % mod:
+#                     raise AssertionError()
+#
+#             for _ in range(10):
+#                 x = random.randrange(0, mod)
+#                 y = random.randrange(0, mod)
+#                 u = mr.convert_in(x)
+#                 v = mr.pow(u, y)
+#                 if mr.convert_out(v) != pow(x, y, mod):
+#                     raise AssertionError()
+
+
+# class MontgomeryReducer:
+#
+#     def __init__(self, mod):
+#         # Modulus
+#         if mod < 3 or mod % 2 == 0:
+#             raise ValueError("Modulus must be an odd number at least 3")
+#         self.modulus = mod
+#
+#         # Reducer
+#         self.reducerbits = (mod.bit_length() // 8 + 1) * 8  # This is a multiple of 8
+#         self.reducer = 1 << self.reducerbits  # This is a power of 256
+#         self.mask = self.reducer - 1
+#         assert self.reducer > mod and math.gcd(self.reducer, mod) == 1
+#
+#         # Other computed numbers
+#         self.reciprocal = MontgomeryReducer.reciprocal_mod(self.reducer % mod, mod)
+#         self.factor = (self.reducer * self.reciprocal - 1) // mod
+#         self.convertedone = self.reducer % mod
+#
+#     # The range of x is unlimited
+#     def convert_in(self, x):
+#         return (x << self.reducerbits) % self.modulus
+#
+#     # The range of x is unlimited
+#     def convert_out(self, x):
+#         return (x * self.reciprocal) % self.modulus
+#
+#     # Inputs and output are in Montgomery form and in the range [0, modulus)
+#     def multiply(self, x, y):
+#         mod = self.modulus
+#         assert 0 <= x < mod and 0 <= y < mod
+#         product = x * y
+#         temp = ((product & self.mask) * self.factor) & self.mask
+#         reduced = (product + temp * mod) >> self.reducerbits
+#         result = reduced if (reduced < mod) else (reduced - mod)
+#         assert 0 <= result < mod
+#         return result
+#
+#     # Input x (base) and output (power) are in Montgomery form and in the range [0, modulus); input y (exponent) is in standard form
+#     def pow(self, x, y):
+#         assert 0 <= x < self.modulus
+#         if y < 0:
+#             raise ValueError("Negative exponent")
+#         z = self.convertedone
+#         while y != 0:
+#             if y & 1 != 0:
+#                 z = self.multiply(z, x)
+#             x = self.multiply(x, x)
+#             y >>= 1
+#         return z
+#
+#     @staticmethod
+#     def reciprocal_mod(x, mod):
+#         # Based on a simplification of the extended Euclidean algorithm
+#         assert mod > 0 and 0 <= x < mod
+#         y = x
+#         x = mod
+#         a = 0
+#         b = 1
+#         while y != 0:
+#             a, b = b, a - x // y * b
+#             x, y = y, x % y
+#         if x == 1:
+#             return a % mod
+#         else:
+#             raise ValueError("Reciprocal does not exist")
+
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
+    #unittest.main()
+    #print(pow(200, 18, 37))
     sys.exit(app.exec_())
