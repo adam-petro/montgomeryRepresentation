02d78c1c9abc55e536ed51838db58087b97fff0e
Escavadora <patrik.bagar@gmail.com>
2020-06-09 18:31:41 +0200

    Added check for odd N and cleaned up code

--- app.py
+++ app.py
@@ -9,10 +9,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 import sys
-import random
-import math
-#import unittest
-#r = 64  #2^6
 
 
 def findClosestPowerOfTwo(n):
@@ -66,7 +62,7 @@ def convertFromMontgomery(x, n):
     res = (int(x) * int(invR)) % int(n)
     result = str(res)
     returnString = (
-        f"<span style=\"font-size:20px\">x: {x}<br>r: {r}<br>n: {n}<br>result: {result}</span>"
+        f"<span style=\"font-size:20px\">x: {x}<br>r: {r}<br>n: {n}<br>result: {result}</span><br><br><br><br><span style=\"font-style:italic; font-size:20px\">{result} = {x}*{r} mod {n}</span>"
     )
     return returnString
 
@@ -80,12 +76,24 @@ def openUnfilledPrompt():
     ''')
     msg.exec_()
 
+def openErrorPrompt():
+    msg = QMessageBox()
+    msg.setWindowTitle("Error")
+    msg.setIcon(QMessageBox.Warning)
+    msg.setText('''
+        <span style=\"font-size:25px\">Value <i><b>N</i></b> must be odd number<span>
+        ''')
+    msg.exec_()
+
 
 def executeConversion():
     x = ui.xInput.text()
     n = ui.nInput.text()
     if (len(n).__eq__(0) or len(x).__eq__(0)):
         openUnfilledPrompt()
+        return
+    if int(n) % 2 == 0:
+        openErrorPrompt()
         return
     if (ui.radioDecToMon.isChecked()):
         result = convertToMontgomery(x, n)
@@ -178,104 +186,10 @@ class Ui_MainWindow(object):
         self.actionl.setText(_translate("MainWindow", "l;"))
 
 
-# class MontgomeryReducerTest(unittest.TestCase):
-#
-#     def test_basic(self):
-#         for _ in range(3000):
-#             bitlen = random.randint(2, 100)
-#             mod = random.randrange(1 << bitlen, 2 << bitlen) | 1  # Force it to be odd
-#             mr = MontgomeryReducer(mod)
-#
-#             for _ in range(100):
-#                 x = random.randrange(0, mod)
-#                 y = random.randrange(0, mod)
-#                 u = mr.convert_in(x)
-#                 v = mr.convert_in(y)
-#                 w = mr.multiply(u, v)
-#                 if mr.convert_out(w) != x * y % mod:
-#                     raise AssertionError()
-#
-#             for _ in range(10):
-#                 x = random.randrange(0, mod)
-#                 y = random.randrange(0, mod)
-#                 u = mr.convert_in(x)
-#                 v = mr.pow(u, y)
-#                 if mr.convert_out(v) != pow(x, y, mod):
-#                     raise AssertionError()
-
-# class MontgomeryReducer:
-#
-#     def __init__(self, mod):
-#         # Modulus
-#         if mod < 3 or mod % 2 == 0:
-#             raise ValueError("Modulus must be an odd number at least 3")
-#         self.modulus = mod
-#
-#         # Reducer
-#         self.reducerbits = (mod.bit_length() // 8 + 1) * 8  # This is a multiple of 8
-#         self.reducer = 1 << self.reducerbits  # This is a power of 256
-#         self.mask = self.reducer - 1
-#         assert self.reducer > mod and math.gcd(self.reducer, mod) == 1
-#
-#         # Other computed numbers
-#         self.reciprocal = MontgomeryReducer.reciprocal_mod(self.reducer % mod, mod)
-#         self.factor = (self.reducer * self.reciprocal - 1) // mod
-#         self.convertedone = self.reducer % mod
-#
-#     # The range of x is unlimited
-#     def convert_in(self, x):
-#         return (x << self.reducerbits) % self.modulus
-#
-#     # The range of x is unlimited
-#     def convert_out(self, x):
-#         return (x * self.reciprocal) % self.modulus
-#
-#     # Inputs and output are in Montgomery form and in the range [0, modulus)
-#     def multiply(self, x, y):
-#         mod = self.modulus
-#         assert 0 <= x < mod and 0 <= y < mod
-#         product = x * y
-#         temp = ((product & self.mask) * self.factor) & self.mask
-#         reduced = (product + temp * mod) >> self.reducerbits
-#         result = reduced if (reduced < mod) else (reduced - mod)
-#         assert 0 <= result < mod
-#         return result
-#
-#     # Input x (base) and output (power) are in Montgomery form and in the range [0, modulus); input y (exponent) is in standard form
-#     def pow(self, x, y):
-#         assert 0 <= x < self.modulus
-#         if y < 0:
-#             raise ValueError("Negative exponent")
-#         z = self.convertedone
-#         while y != 0:
-#             if y & 1 != 0:
-#                 z = self.multiply(z, x)
-#             x = self.multiply(x, x)
-#             y >>= 1
-#         return z
-#
-#     @staticmethod
-#     def reciprocal_mod(x, mod):
-#         # Based on a simplification of the extended Euclidean algorithm
-#         assert mod > 0 and 0 <= x < mod
-#         y = x
-#         x = mod
-#         a = 0
-#         b = 1
-#         while y != 0:
-#             a, b = b, a - x // y * b
-#             x, y = y, x % y
-#         if x == 1:
-#             return a % mod
-#         else:
-#             raise ValueError("Reciprocal does not exist")
-
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
-    #unittest.main()
-    #print(pow(200, 18, 37))
     sys.exit(app.exec_())
